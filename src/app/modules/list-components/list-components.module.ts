ebe8a6e5a513e4236f52be4832658ca44890d6c7
Guilherme Nunes da Silva <guilherme.nunes@fover.com.br>
2022-10-25 13:28:19 -0300

    Iniciando tela dos componentes

--- src/app/modules/list-components/list-components.module.ts
+++ src/app/modules/list-components/list-components.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListComponentsDefaultPage } from './pages/default/list-components-default.page';
 import { ListComponentsRoutingModule } from './list-components-routing.module';
+import { MenuLateralComponent } from './pages/default/components/menu-lateral/menu-lateral.component';
 
 @NgModule({
     declarations: [
         ListComponentsDefaultPage,
+        MenuLateralComponent,
     ],
     imports: [
         CommonModule,
