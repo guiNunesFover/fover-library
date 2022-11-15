f451bbb92374e9d3ee938c859ba374c6effe99e6
Guilherme Nunes da Silva <guilherme.nunes@fover.com.br>
2022-11-15 14:47:07 -0300

    Iniciando a incersão dos icones

---
+++ projects/fover-library-lib/src/lib/assets/icons/icons-font/icons.ts
@@ -0,0 +1,317 @@
+export type IconsId =
+  | "archive"
+  | "card-add"
+  | "card-edit"
+  | "card-pos"
+  | "card-receive"
+  | "card-remove-2"
+  | "card-remove"
+  | "card-send"
+  | "card-slash"
+  | "card-tick-2"
+  | "card-tick"
+  | "card"
+  | "cards"
+  | "chart-square"
+  | "coin-2"
+  | "coin"
+  | "convert-card"
+  | "discount-circle"
+  | "discount-shape"
+  | "document"
+  | "dollar-circle"
+  | "dollar-square"
+  | "empty-wallet-add"
+  | "empty-wallet-change"
+  | "empty-wallet-remove"
+  | "empty-wallet-tick"
+  | "empty-wallet-time"
+  | "empty-wallet"
+  | "math"
+  | "money-2"
+  | "money-3"
+  | "money-4"
+  | "money-add"
+  | "money-change"
+  | "money-forbidden"
+  | "money-recive"
+  | "money-remove"
+  | "money-send"
+  | "money-tick"
+  | "money-time"
+  | "money"
+  | "moneys"
+  | "percentage-circle"
+  | "percentage-square"
+  | "receipt-2"
+  | "receipt-3"
+  | "receipt-4"
+  | "receipt-add"
+  | "receipt-discount"
+  | "receipt-disscount"
+  | "receipt-edit"
+  | "receipt-item"
+  | "receipt-minus"
+  | "receipt-search"
+  | "receipt-text"
+  | "receipt"
+  | "security-card"
+  | "strongbox-2"
+  | "strongbox"
+  | "tag-2"
+  | "tag"
+  | "ticket-2"
+  | "ticket-discount"
+  | "ticket-expired"
+  | "ticket-star"
+  | "ticket"
+  | "transaction-minus"
+  | "wallet-2"
+  | "wallet-3"
+  | "wallet-4"
+  | "wallet-add-2"
+  | "wallet-add"
+  | "wallet-check"
+  | "wallet-money"
+  | "wallet-remove"
+  | "wallet-search"
+  | "wallet";
+
+export type IconsKey =
+  | "Archive"
+  | "CardAdd"
+  | "CardEdit"
+  | "CardPos"
+  | "CardReceive"
+  | "CardRemove_2"
+  | "CardRemove"
+  | "CardSend"
+  | "CardSlash"
+  | "CardTick_2"
+  | "CardTick"
+  | "Card"
+  | "Cards"
+  | "ChartSquare"
+  | "Coin_2"
+  | "Coin"
+  | "ConvertCard"
+  | "DiscountCircle"
+  | "DiscountShape"
+  | "Document"
+  | "DollarCircle"
+  | "DollarSquare"
+  | "EmptyWalletAdd"
+  | "EmptyWalletChange"
+  | "EmptyWalletRemove"
+  | "EmptyWalletTick"
+  | "EmptyWalletTime"
+  | "EmptyWallet"
+  | "Math"
+  | "Money_2"
+  | "Money_3"
+  | "Money_4"
+  | "MoneyAdd"
+  | "MoneyChange"
+  | "MoneyForbidden"
+  | "MoneyRecive"
+  | "MoneyRemove"
+  | "MoneySend"
+  | "MoneyTick"
+  | "MoneyTime"
+  | "Money"
+  | "Moneys"
+  | "PercentageCircle"
+  | "PercentageSquare"
+  | "Receipt_2"
+  | "Receipt_3"
+  | "Receipt_4"
+  | "ReceiptAdd"
+  | "ReceiptDiscount"
+  | "ReceiptDisscount"
+  | "ReceiptEdit"
+  | "ReceiptItem"
+  | "ReceiptMinus"
+  | "ReceiptSearch"
+  | "ReceiptText"
+  | "Receipt"
+  | "SecurityCard"
+  | "Strongbox_2"
+  | "Strongbox"
+  | "Tag_2"
+  | "Tag"
+  | "Ticket_2"
+  | "TicketDiscount"
+  | "TicketExpired"
+  | "TicketStar"
+  | "Ticket"
+  | "TransactionMinus"
+  | "Wallet_2"
+  | "Wallet_3"
+  | "Wallet_4"
+  | "WalletAdd_2"
+  | "WalletAdd"
+  | "WalletCheck"
+  | "WalletMoney"
+  | "WalletRemove"
+  | "WalletSearch"
+  | "Wallet";
+
+export enum Icons {
+  Archive = "archive",
+  CardAdd = "card-add",
+  CardEdit = "card-edit",
+  CardPos = "card-pos",
+  CardReceive = "card-receive",
+  CardRemove_2 = "card-remove-2",
+  CardRemove = "card-remove",
+  CardSend = "card-send",
+  CardSlash = "card-slash",
+  CardTick_2 = "card-tick-2",
+  CardTick = "card-tick",
+  Card = "card",
+  Cards = "cards",
+  ChartSquare = "chart-square",
+  Coin_2 = "coin-2",
+  Coin = "coin",
+  ConvertCard = "convert-card",
+  DiscountCircle = "discount-circle",
+  DiscountShape = "discount-shape",
+  Document = "document",
+  DollarCircle = "dollar-circle",
+  DollarSquare = "dollar-square",
+  EmptyWalletAdd = "empty-wallet-add",
+  EmptyWalletChange = "empty-wallet-change",
+  EmptyWalletRemove = "empty-wallet-remove",
+  EmptyWalletTick = "empty-wallet-tick",
+  EmptyWalletTime = "empty-wallet-time",
+  EmptyWallet = "empty-wallet",
+  Math = "math",
+  Money_2 = "money-2",
+  Money_3 = "money-3",
+  Money_4 = "money-4",
+  MoneyAdd = "money-add",
+  MoneyChange = "money-change",
+  MoneyForbidden = "money-forbidden",
+  MoneyRecive = "money-recive",
+  MoneyRemove = "money-remove",
+  MoneySend = "money-send",
+  MoneyTick = "money-tick",
+  MoneyTime = "money-time",
+  Money = "money",
+  Moneys = "moneys",
+  PercentageCircle = "percentage-circle",
+  PercentageSquare = "percentage-square",
+  Receipt_2 = "receipt-2",
+  Receipt_3 = "receipt-3",
+  Receipt_4 = "receipt-4",
+  ReceiptAdd = "receipt-add",
+  ReceiptDiscount = "receipt-discount",
+  ReceiptDisscount = "receipt-disscount",
+  ReceiptEdit = "receipt-edit",
+  ReceiptItem = "receipt-item",
+  ReceiptMinus = "receipt-minus",
+  ReceiptSearch = "receipt-search",
+  ReceiptText = "receipt-text",
+  Receipt = "receipt",
+  SecurityCard = "security-card",
+  Strongbox_2 = "strongbox-2",
+  Strongbox = "strongbox",
+  Tag_2 = "tag-2",
+  Tag = "tag",
+  Ticket_2 = "ticket-2",
+  TicketDiscount = "ticket-discount",
+  TicketExpired = "ticket-expired",
+  TicketStar = "ticket-star",
+  Ticket = "ticket",
+  TransactionMinus = "transaction-minus",
+  Wallet_2 = "wallet-2",
+  Wallet_3 = "wallet-3",
+  Wallet_4 = "wallet-4",
+  WalletAdd_2 = "wallet-add-2",
+  WalletAdd = "wallet-add",
+  WalletCheck = "wallet-check",
+  WalletMoney = "wallet-money",
+  WalletRemove = "wallet-remove",
+  WalletSearch = "wallet-search",
+  Wallet = "wallet",
+}
+
+export const ICONS_CODEPOINTS: { [key in Icons]: string } = {
+  [Icons.Archive]: "61697",
+  [Icons.CardAdd]: "61698",
+  [Icons.CardEdit]: "61699",
+  [Icons.CardPos]: "61700",
+  [Icons.CardReceive]: "61701",
+  [Icons.CardRemove_2]: "61702",
+  [Icons.CardRemove]: "61703",
+  [Icons.CardSend]: "61704",
+  [Icons.CardSlash]: "61705",
+  [Icons.CardTick_2]: "61706",
+  [Icons.CardTick]: "61707",
+  [Icons.Card]: "61708",
+  [Icons.Cards]: "61709",
+  [Icons.ChartSquare]: "61710",
+  [Icons.Coin_2]: "61711",
+  [Icons.Coin]: "61712",
+  [Icons.ConvertCard]: "61713",
+  [Icons.DiscountCircle]: "61714",
+  [Icons.DiscountShape]: "61715",
+  [Icons.Document]: "61716",
+  [Icons.DollarCircle]: "61717",
+  [Icons.DollarSquare]: "61718",
+  [Icons.EmptyWalletAdd]: "61719",
+  [Icons.EmptyWalletChange]: "61720",
+  [Icons.EmptyWalletRemove]: "61721",
+  [Icons.EmptyWalletTick]: "61722",
+  [Icons.EmptyWalletTime]: "61723",
+  [Icons.EmptyWallet]: "61724",
+  [Icons.Math]: "61725",
+  [Icons.Money_2]: "61726",
+  [Icons.Money_3]: "61727",
+  [Icons.Money_4]: "61728",
+  [Icons.MoneyAdd]: "61729",
+  [Icons.MoneyChange]: "61730",
+  [Icons.MoneyForbidden]: "61731",
+  [Icons.MoneyRecive]: "61732",
+  [Icons.MoneyRemove]: "61733",
+  [Icons.MoneySend]: "61734",
+  [Icons.MoneyTick]: "61735",
+  [Icons.MoneyTime]: "61736",
+  [Icons.Money]: "61737",
+  [Icons.Moneys]: "61738",
+  [Icons.PercentageCircle]: "61739",
+  [Icons.PercentageSquare]: "61740",
+  [Icons.Receipt_2]: "61741",
+  [Icons.Receipt_3]: "61742",
+  [Icons.Receipt_4]: "61743",
+  [Icons.ReceiptAdd]: "61744",
+  [Icons.ReceiptDiscount]: "61745",
+  [Icons.ReceiptDisscount]: "61746",
+  [Icons.ReceiptEdit]: "61747",
+  [Icons.ReceiptItem]: "61748",
+  [Icons.ReceiptMinus]: "61749",
+  [Icons.ReceiptSearch]: "61750",
+  [Icons.ReceiptText]: "61751",
+  [Icons.Receipt]: "61752",
+  [Icons.SecurityCard]: "61753",
+  [Icons.Strongbox_2]: "61754",
+  [Icons.Strongbox]: "61755",
+  [Icons.Tag_2]: "61756",
+  [Icons.Tag]: "61757",
+  [Icons.Ticket_2]: "61758",
+  [Icons.TicketDiscount]: "61759",
+  [Icons.TicketExpired]: "61760",
+  [Icons.TicketStar]: "61761",
+  [Icons.Ticket]: "61762",
+  [Icons.TransactionMinus]: "61763",
+  [Icons.Wallet_2]: "61764",
+  [Icons.Wallet_3]: "61765",
+  [Icons.Wallet_4]: "61766",
+  [Icons.WalletAdd_2]: "61767",
+  [Icons.WalletAdd]: "61768",
+  [Icons.WalletCheck]: "61769",
+  [Icons.WalletMoney]: "61770",
+  [Icons.WalletRemove]: "61771",
+  [Icons.WalletSearch]: "61772",
+  [Icons.Wallet]: "61773",
+};
