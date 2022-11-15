0c96a409d86ae92e93b80a047d6dfcbb6ca3c645
Guilherme Nunes da Silva <guilherme.nunes@fover.com.br>
2022-11-15 18:00:11 -0300

    Icones na direita

--- src/app/modules/list-components/pages/default/pages/inputs/input.page.ts
+++ src/app/modules/list-components/pages/default/pages/inputs/input.page.ts
@@ -9,10 +9,12 @@ export class InputPage
 {
     public codeSimple: string;
     public codeWithLabel: string;
+    public codeIconRight: string;
 
     constructor() 
     {
         this.codeSimple = require('html-loader!./examples/simple.html').default;
         this.codeWithLabel = require('html-loader!./examples/with-label.html').default;
+        this.codeIconRight = require('html-loader!./examples/icon-right.html').default;
     }
 }
