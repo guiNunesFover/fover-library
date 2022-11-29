22ca0df31b2a6b20bc44132ba0704e964297ca12
Guilherme Nunes da Silva <guilherme.nunes@fover.com.br>
2022-11-29 11:17:37 -0300

    Nova versão

--- projects/fover-library-lib/src/lib/modules/components/index.ts
+++ projects/fover-library-lib/src/lib/modules/components/index.ts
@@ -2,4 +2,4 @@ export { FoverInputComponent, FoverInputModule } from "./input";
 export { FoverButtonComponent, FoverButtonModule } from "./button";
 export { FoverAlertComponent, FoverAlertModule } from "./alert";
 export { FoverNotificationAreaComponent, FoverNotificationCardComponent, FoverNotificationModel, FoverNotificationModule, FoverNotificationService, FoverNotificationStatusEnum, } from "./notification";
-export { FoverComponentesModule } from "./fover-componentes.module";
+export { FoverComponentsModule } from "./fover-componentes.module";
