c7e2176a63fd4784fe59d9775129de49c415fc83
Guilherme Nunes da Silva <guilherme.nunes@fover.com.br>
2022-12-01 15:55:26 -0300

    Melhoria na listagem do menu

--- src/app/modules/list-components/pages/default/components/menu-lateral/menu-lateral.component.ts
+++ src/app/modules/list-components/pages/default/components/menu-lateral/menu-lateral.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'menu-lateral',
     templateUrl: './menu-lateral.component.html',
     styleUrls: ['./menu-lateral.component.scss']
 })
-export class MenuLateralComponent {}
+export class MenuLateralComponent implements OnInit
+{
+    public listItens: { routerLink: string, description: string }[] = [];
+
+    ngOnInit(): void 
+    {
+        this.buildList();
+    }
+    
+    private buildList(): void
+    {
+        this.listItens = [
+            { routerLink: "buttons", description: "Buttons" },
+            { routerLink: "inputs", description: "Inputs" },
+            { routerLink: "alerts", description: "Alerts" },
+            { routerLink: "notifications", description: "Notifications" },
+            { routerLink: "loadings", description: "Loadings" },
+            { routerLink: "modals", description: "Modals" },
+        ];
+
+        this.listItens.sort((a, b) => (a.description > b.description) ? 1 : -1);
+    }
+}
